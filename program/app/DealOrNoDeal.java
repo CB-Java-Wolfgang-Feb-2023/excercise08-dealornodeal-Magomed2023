930233febdf5d471c0d6c85b3a904aad9cb2d311
Magomed2023 <m.muzhakhoev@gmx.at>
2023-10-12 20:37:33 +0200

    Update Final

--- program/app/DealOrNoDeal.java
+++ program/app/DealOrNoDeal.java
@@ -6,7 +6,7 @@ public class DealOrNoDeal {
 
     private final double[] caseValues = {0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000};
 
-    // Map of case number to case value
+
     private final Map<Integer, Double> cases = new HashMap<>();
 
 
